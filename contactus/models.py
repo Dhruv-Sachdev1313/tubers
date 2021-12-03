94e2ff7d18d205c5ec927656d4f134cc71827db3
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-12-03 19:50:26 +0530

    added data model
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- contactus/models.py
+++ contactus/models.py
@@ -1,7 +1,19 @@
 from django.db import models
 from datetime import datetime
 
+
 # Create your models here.
 
 class Contactus(models.Model):
-    first_name = models.CharField()
+    full_name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, blank=True)
+    subject = models.TextField()
+    message = models.TextField()
+    created_at = models.DateTimeField(blank=True, default=datetime.now)
+    user_id = models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.email
+
--- contactus/views.py
+++ contactus/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import Contactus
+from django.contrib import messages
 
 # Create your views here.
+
+def contactus(request):
+    if request.method == 'POST':
+        full_name = request.POST['full_name']
+        phone = request.POST['phone']
+        email = request.POST['email']
+        company_name = request.POST['company_name']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        user_id = request.POST['user_id']
+        #TODO: Sanitisation
+
+        contactus = Contactus(full_name=full_name, phone=phone, email=email, company_name=company_name, subject=subject, message=message, user_id=user_id)
+        contactus.save()
+        messages.success(request, 'Thanks for reaching out!')
+        return redirect('contact')
+
+
