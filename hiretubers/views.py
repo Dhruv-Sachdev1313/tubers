b37ee6163bf43aadd8f75c5747f6bec039660725
Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>
2021-12-03 19:51:08 +0530

    fixed hiretubber view POST request
    Signed-off-by: Dhruv-Sachdev1313 <dhruvhsachdev@gmail.com>

--- hiretubers/views.py
+++ hiretubers/views.py
@@ -4,9 +4,9 @@ from django.contrib import messages
 
 # Create your views here.
 def hiretuber(request):
-    print('method called')
+   
     if request.method == 'POST':
-        print('request recieved!')
+        
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         tuber_id = request.POST['tuber_id']
@@ -20,9 +20,7 @@ def hiretuber(request):
 
         # TODO: do all sanitisation
         hiretuber = Hiretuber(first_name=first_name, last_name=last_name, tuber_id=tuber_id, tuber_name= tuber_name, email=email,city=city, phone=phone, state=state, user_id=user_id, message= message)
-        print('instance created')
+
         hiretuber.save()
-        print('instance saved')
         messages.success(request, 'Thanks for reaching out!')
-        print('success')
         return redirect('youtubers')
